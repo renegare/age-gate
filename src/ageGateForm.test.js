6ceb204eb64a7ba53159cf2b807c76b2df71312a
renegare <mudi@renegare.com>
2019-06-09 22:48:28 +0100

    wip ageGateForm

---
+++ src/ageGateForm.test.js
@@ -0,0 +1,15 @@
+describe("ageGateForm", () => {
+  it("should display form");
+
+  describe("confirm", () => {
+    it("should set cookie");
+
+    it("should close form");
+  });
+
+  describe("cancel", () => {
+    it("should not set the cookie");
+
+    it("should close form");
+  });
+});
