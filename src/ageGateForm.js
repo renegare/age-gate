4ad96ba4f16074873bda73837180e12b4e58f150
renegare <mudi@renegare.com>
2019-06-10 14:40:27 +0100

    remove useless dom structure

--- src/ageGateForm.js
+++ src/ageGateForm.js
@@ -1,49 +1,5 @@
 const createElements = require("./createElements");
 const insertAfter = require("./insertAfter");
-const AG_DOM_STRUCTURE = [
-  {
-    tag: "div",
-    className: "ag-container",
-    children: [
-      {
-        tag: "div",
-        className: "ag-modal",
-        children: [
-          {
-            tag: "div",
-            className: "ag-content",
-            children: [
-              {
-                tag: "h1",
-                children: ["Age Restricted Content"]
-              },
-              {
-                tag: "p",
-                children: [
-                  "Please confirm you are above the legal drinking age in your country"
-                ]
-              }
-            ]
-          },
-          {
-            tag: "div",
-            className: "ag-options",
-            children: [
-              {
-                tag: "button",
-                className: "ag-confirm"
-              },
-              {
-                tag: "button",
-                className: "ag-cancel"
-              }
-            ]
-          }
-        ]
-      }
-    ]
-  }
-];
 
 const display = () => {
   const dom = document.createElement("div");
--- src/ageGateForm.test.js
+++ src/ageGateForm.test.js
@@ -6,51 +6,6 @@ const form = require("./ageGateForm");
 jest.mock("./createElements", () => jest.fn());
 jest.mock("./insertAfter", () => jest.fn());
 
-const EXPECTED_DOM_STRUCTURE = [
-  {
-    tag: "div",
-    className: "ag-container",
-    children: [
-      {
-        tag: "div",
-        className: "ag-modal",
-        children: [
-          {
-            tag: "div",
-            className: "ag-content",
-            children: [
-              {
-                tag: "h1",
-                children: ["Age Restricted Content"]
-              },
-              {
-                tag: "p",
-                children: [
-                  "Please confirm you are above the legal drinking age in your country"
-                ]
-              }
-            ]
-          },
-          {
-            tag: "div",
-            className: "ag-options",
-            children: [
-              {
-                tag: "button",
-                className: "ag-confirm"
-              },
-              {
-                tag: "button",
-                className: "ag-cancel"
-              }
-            ]
-          }
-        ]
-      }
-    ]
-  }
-];
-
 describe("ageGateForm", () => {
   it("should display form", () => {
     form.display();
