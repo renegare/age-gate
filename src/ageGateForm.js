9e79f3409d85522c473b131c63f2176c9341a0eb
renegare <mudi@renegare.com>
2019-06-10 16:44:13 +0100

    completed logic for gate dom

--- src/ageGateForm.js
+++ src/ageGateForm.js
@@ -1,23 +1,68 @@
 const createElements = require("./createElements");
 const insertAfter = require("./insertAfter");
 
-const display = () => {
+const ROLE_CONFIRM = "confirm";
+const ROLE_CANCEL = "cancel";
+const ROLE_CLOSE = "close";
+
+const EVENT_LOADED = "loaded";
+const EVENT_CONFIRMED = "confirmed";
+const EVENT_CANCELLED = "cancelled";
+const EVENT_CLOSED = "closed";
+
+const noOp = () => {};
+
+const display = cb => {
+  cb = cb || noOp;
+
   const dom = document.createElement("div");
   dom.innerHTML = `<div class="ag-modal-container">
-    <div class="ag-confirm-modal visible">
+    <div class="ag-confirm-modal">
       <div class="ag-content">
         <h1>Age Restricted Content</h1>
         <p>Please confirm you are above the legal drinking age in your country</p>
       </div>
 
       <div class="ag-options">
-        <button class="ag-confirm">Confirm</button>
-        <button class="ag-cancel">Cancel</button>
+        <button class="ag-confirm" role="${ROLE_CONFIRM}">Confirm</button>
+        <button class="ag-cancel" role="${ROLE_CANCEL}">Cancel</button>
       </div>
     </div>
-    <div class="ag-cancel-modal hidden"></div>
+    <div class="ag-cancel-modal hidden">
+      <div class="ag-content">
+        <h1>Sorry!</h1>
+        <p>You need to be of legal drinking age to visit our website</p>
+      </div>
+      <div class="ag-options">
+        <button class="ag-close" role="${ROLE_CLOSE}">Close</button>
+      </div>
+    </div>
   </div>`;
-  insertAfter(dom);
+
+  const confirmModal = dom.querySelector(".ag-confirm-modal");
+  const cancelModal = dom.querySelector(".ag-cancel-modal");
+
+  dom.addEventListener("click", (a, b, c) => {
+    const role = a.target.getAttribute("role");
+    switch (role) {
+      case ROLE_CONFIRM:
+        confirmModal.classList.add("hidden");
+        cb(EVENT_CONFIRMED, dom);
+        break;
+      case ROLE_CANCEL:
+        confirmModal.classList.add("hidden");
+        cancelModal.classList.remove("hidden");
+        cb(EVENT_CANCELLED, dom);
+        break;
+      case ROLE_CLOSE:
+        confirmModal.classList.add("hidden");
+        cancelModal.classList.add("hidden");
+        cb(EVENT_CLOSED, dom);
+        break;
+    }
+  });
+
+  cb(EVENT_LOADED, dom);
 };
 
 module.exports = {
--- src/ageGateForm.test.js
+++ src/ageGateForm.test.js
@@ -6,18 +6,61 @@ const form = require("./ageGateForm");
 jest.mock("./createElements", () => jest.fn());
 jest.mock("./insertAfter", () => jest.fn());
 
+const callback = jest.fn();
+
+expect.extend({
+  toBeVisible: dom => {
+    try {
+      expect(dom).toEqual(expect.any(Element));
+    } catch (e) {
+      return {
+        message: () => `expected DOM Element but received "${typeof dom}"`,
+        pass: false
+      };
+    }
+
+    return {
+      message: () =>
+        `expected DOM with class(es) "${[].slice
+          .call(dom.classList)
+          .join(" ")}" to not contain the class hidden`,
+      pass: !dom.classList.contains("hidden")
+    };
+  }
+});
+
 describe("ageGateForm", () => {
+  let dom, $html;
+  beforeEach(() => {
+    jest.resetAllMocks();
+    form.display(callback);
+
+    dom = callback.mock.calls[0][1];
+    $html = $(dom);
+  });
+
+  it("should trigger callback with loaded event", () => {
+    expect(callback).toHaveBeenCalledTimes(1);
+    expect(callback).toHaveBeenCalledWith("loaded", dom);
+  });
+
+  afterEach(() => {
+    dom = null;
+    $html = null;
+  });
+
   it("should display form", () => {
-    form.display();
-
-    expect(insertAfter).toHaveBeenCalledTimes(1);
-    expect(insertAfter).toHaveBeenCalledWith(expect.any(Element));
-
-    const $html = $(insertAfter.mock.calls[0]);
+    const $html = $(dom);
 
     expect($html.find(".ag-modal-container")).toHaveLength(1);
 
-    expect($html.find(".ag-confirm-modal.visible")).toHaveLength(1);
+    $confirmModal = $html.find(".ag-confirm-modal");
+    expect($confirmModal).toHaveLength(1);
+    expect($confirmModal[0]).toBeVisible();
+
+    $cancelModal = $html.find(".ag-cancel-modal");
+    expect($cancelModal).toHaveLength(1);
+    expect($cancelModal[0]).not.toBeVisible();
 
     expect($html.find(".ag-confirm-modal h1")).toHaveLength(1);
     expect($html.find(".ag-confirm-modal h1").text()).toEqual(
@@ -28,21 +71,76 @@ describe("ageGateForm", () => {
       "Please confirm you are above the legal drinking age in your country"
     );
 
-    expect($html.find(".ag-options .ag-confirm")).toHaveLength(1);
-    expect($html.find(".ag-options .ag-cancel")).toHaveLength(1);
-
-    expect($html.find(".ag-cancel-modal.hidden")).toHaveLength(1);
+    expect($html.find(".ag-confirm-modal .ag-confirm")).toHaveLength(1);
+    expect($html.find(".ag-confirm-modal .ag-cancel")).toHaveLength(1);
   });
 
-  // describe("confirm", () => {
-  //   it("should set cookie");
+  describe("confirm", () => {
+    it("should hide modal", () => {
+      callback.mockReset();
 
-  //   it("should close form");
-  // });
+      const $button = $html.find(".ag-options .ag-confirm");
 
-  // describe("cancel", () => {
-  //   it("should not set the cookie");
+      $button.click();
+      $confirmModal = $html.find(".ag-confirm-modal");
+      expect($confirmModal[0]).not.toBeVisible();
 
-  //   it("should close form");
-  // });
+      $cancelModal = $html.find(".ag-cancel-modal");
+      expect($cancelModal[0]).not.toBeVisible();
+
+      expect(callback).toHaveBeenCalledTimes(1);
+      expect(callback).toHaveBeenCalledWith("confirmed", dom);
+    });
+  });
+
+  describe("cancel", () => {
+    beforeEach(() => {
+      callback.mockReset();
+      const $button = $html.find(".ag-options .ag-cancel");
+      $button.click();
+    });
+
+    it("should trigger cancelled event", () => {
+      expect(callback).toHaveBeenCalledTimes(1);
+      expect(callback).toHaveBeenCalledWith("cancelled", dom);
+    });
+
+    it("should display cancel modal form", () => {
+      $confirmModal = $html.find(".ag-confirm-modal");
+      expect($confirmModal[0]).not.toBeVisible();
+
+      $cancelModal = $html.find(".ag-cancel-modal");
+      expect($cancelModal[0]).toBeVisible();
+
+      expect($html.find(".ag-cancel-modal h1")).toHaveLength(1);
+      expect($html.find(".ag-cancel-modal h1").text()).toEqual("Sorry!");
+      expect($html.find(".ag-cancel-modal p")).toHaveLength(1);
+      expect($html.find(".ag-cancel-modal p").text()).toEqual(
+        "You need to be of legal drinking age to visit our website"
+      );
+
+      expect($html.find(".ag-cancel-modal .ag-close")).toHaveLength(1);
+    });
+
+    describe("close", () => {
+      beforeEach(() => {
+        callback.mockReset();
+        const $button = $html.find(".ag-cancel-modal .ag-close");
+        $button.click();
+      });
+
+      it("should trigger close event", () => {
+        expect(callback).toHaveBeenCalledTimes(1);
+        expect(callback).toHaveBeenCalledWith("closed", dom);
+      });
+
+      it("should hide modal", () => {
+        $confirmModal = $html.find(".ag-confirm-modal");
+        expect($confirmModal[0]).not.toBeVisible();
+
+        $cancelModal = $html.find(".ag-cancel-modal");
+        expect($cancelModal[0]).not.toBeVisible();
+      });
+    });
+  });
 });
